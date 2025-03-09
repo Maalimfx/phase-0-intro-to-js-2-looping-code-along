1f01a591af040bef496f9a08c896189c2f592b8f
abass <abass@DESKTOP-478KRS4.>
2025-03-09 18:00:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+// for( let age = 30; age < 40; age++ ){
+//     console.log(`i'm ${age} years old. Happy birthday to me!`);
+   
+// }
+
+// let names =["Guadalupe","Ollie", " Aki" ];
+// function writeCards(names, event){
+  
+//     for(let i= 0; i < names.length; i++){
+
+//         console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+//     }
+//     console.log(answer)
+// }
+function writeCards(names, event) {
+    let output = [];
+    for (let i = 0; i < names.length; i++) {
+        output.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return output;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
+
+
